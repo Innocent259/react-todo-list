8ae3ae526f8d7cda28478b3773a078306f26ec8c
innocent259 <ndahimanainnocent842@gmail.com>
2023-07-11 22:33:32 +0200

    Display todo list

--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
-export default function TodoItem() {
+export default function TodoItem({ itemProps, handleChange, delTodo }) {
   return (
     <div>
-      
+      <li>
+      <input
+        type="checkbox"
+        checked={itemProps.completed}
+        onChange={() => handleChange(itemProps.id)}
+      />
+      <button onClick={() => delTodo(itemProps.id)}>Delete</button>
+      {itemProps.title}
+      </li>
     </div>
   )
 }
