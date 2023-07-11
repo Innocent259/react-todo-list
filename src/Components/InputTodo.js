a877b187bccbeddbd6a2bfa1fe62cd7fa0c61a5f
innocent259 <ndahimanainnocent842@gmail.com>
2023-07-11 22:33:04 +0200

    Complete Input component

--- src/Components/InputTodo.js
+++ src/Components/InputTodo.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import { useState } from 'react'
 
-export default function InputTodo() {
+export default function InputTodo({ addTodoItem }) {
+  const [title, setTitle] = useState('')
+  const [message, setMessage] = useState('')
+  const handleChange = (e) => {
+    setTitle(e.target.value)
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (title.trim()) {
+      addTodoItem(title);
+      setTitle('');
+      setMessage('');
+    } else {
+      setMessage('Please add item');
+    }
+  };
   return (
     <div>
-      <h1>form input will be here</h1>
+      <form onSubmit={handleSubmit}>
+        <input 
+        type="text"
+        value={title}
+        onChange={handleChange}
+        placeholder="Add Todo..."
+        />
+        <button>Submit</button>
+    </form>
+    <span>{message}</span>
     </div>
   )
 }
