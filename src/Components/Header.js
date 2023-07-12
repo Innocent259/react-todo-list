4d2ef9d3aaebe638008d7e17960e162df88f0fe6
innocent259 <ndahimanainnocent842@gmail.com>
2023-07-12 08:08:20 +0200

    Modify the header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import styles from '../styles/Header.module.css';
+
 
 export default function Header() {
   return (
-    <div>
+    <header className={styles.header}>
       <h1>todos</h1>
       <p>Items will persist in the browser local storage</p>
-    </div>
+    </header>
   )
 }
